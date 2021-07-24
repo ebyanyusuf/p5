9bd420412ced99a37d4db933037e0082324b88ef
ebyanyusuf <ebyan.yusuf@gmail.com>
2021-07-24 16:18:46 -0400

    second

--- start.js
+++ start.js
@@ -1,1 +1,6 @@
-start
+function getSecondLargest(nums) {
+
+    var max = Math.max.apply(null, nums); // get the max of the array
+    nums.splice(nums.indexOf(max), 1); // remove max from the array
+    return Math.max.apply(null, nums); // get the 2nd max
+}
